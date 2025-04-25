492c6cfdd4b3e71e831437212e73b910d2a2702c
Michael Nobles <Mnobles33@gmail.com>
2025-04-25 16:02:09 -0700

    Next commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,26 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const User = require("../users/users-model");
 
 router.post("/register", validateRoleName, (req, res, next) => {
+  let { username, password, role_name } = req.body;
+
+  // username = username.trim();
+  // password = password.trim();
+  // role_name = role_name.trim();
+
+  username.add(username)
+  password.add(password)
+  role_name.add(role_name)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(next)
+})
+
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -14,10 +32,27 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
-});
+
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let { username, password } = req.body;
+
+  User.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = jwt.sign(
+          { subject: user.id, username: user.username, role_name: user.role_name },
+          JWT_SECRET,
+          { expiresIn: "1 day" }
+        );
+        res.status(200).json({ message: `${user.username} is back!`, token });
+      } else {
+        next({ status: 401, message: "Invalid credentials" });
+      }
+    })
+    .catch(next);
+
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 

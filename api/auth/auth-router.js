cef0e3516fed198853d9008e4d823456602c7684
Michael Nobles <Mnobles33@gmail.com>
2025-04-25 16:13:20 -0700

    next commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!

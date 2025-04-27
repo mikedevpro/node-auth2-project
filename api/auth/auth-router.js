5cba941eda1ec049966b3ca1ef634d59649c457b
Michael Nobles <Mnobles33@gmail.com>
2025-04-26 17:16:03 -0700

    next commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,20 +6,25 @@ const { JWT_SECRET } = require("../secrets"); // use this secret!
 const User = require("../users/users-model");
 
 router.post("/register", validateRoleName, (req, res, next) => {
-  let { username, password, role_name } = req.body;
+  const { username, password } = req.body;
+  const { role_name } = req;
+  const hash = bcrypt.hashSync(password, 10);
+
+  User.add({ username, password: hash, role_name })
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(next)
+})
 
   // username = username.trim();
   // password = password.trim();
   // role_name = role_name.trim();
 
-  username.add(username)
-  password.add(password)
-  role_name.add(role_name)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(next)
-})
+  // username.add(username)
+  // password.add(password)
+  // role_name.add(role_name)
+    
 
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
@@ -44,7 +49,7 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
         const token = jwt.sign(
           { subject: user.id, username: user.username, role_name: user.role_name },
           JWT_SECRET,
-          { expiresIn: "1 day" }
+          { exp: "1 day" }
         );
         res.status(200).json({ message: `${user.username} is back!`, token });
       } else {

6a3081eaf796cee7bde4f4dd3bb25741e8d2586a
Michael Nobles <Mnobles33@gmail.com>
2025-04-27 16:19:26 -0700

    next commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,21 +1,25 @@
 // const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-// const User = require("../users/users-model");
+const User = require("../users/users-model");
 
 router.post("/register", validateRoleName, (req, res, next) => {
   const { username, password } = req.body;
   const { role_name } = req;
-  const hash = bcrypt.hashSync(password, 10);
+  const hash = bcrypt.hashSync(password, 8);
 
   User.add({ username, password: hash, role_name })
-  .then(saved => {
-    res.status(201).json(saved);
-  })
-  .catch(next)
-})
+    .then(newUser => {
+      res.status(201).json({
+        user: newUser.user,
+        username: newUser.username,
+        role_name: newUser.role_name,
+      })
+    })
+    .catch(next)
+});
 
   // username = username.trim();
   // password = password.trim();
